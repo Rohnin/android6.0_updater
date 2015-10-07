bb09275b47bfe4155fce93cbcbff855986587f3f
Krystian <magiera.krystian@gmail.com>
2015-10-07 18:13:23 +0200

    autoupdater for android 6.0

---
+++ update.bat
@@ -0,0 +1,15 @@
+fastboot erase boot
+fastboot erase cache
+fastboot erase recovery
+fastboot erase system
+fastboot flash bootloader bootloader.img
+fastboot reboot-bootloader
+fastboot flash radio radio.img
+fastboot reboot-bootloader
+fastboot flash system system.img
+fastboot flash boot boot.img
+fastboot flash recovery recovery.img
+fastboot flash cache cache.img
+fastboot erase userdata
+fastboot flash userdata userdata.img
+fastboot reboot
